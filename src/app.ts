7605d99104a595d368d8d302fe6450c6d463658b
Saidur289 <saidur.riaz1@gmailcomm>
2026-01-06 19:36:18 +0600

    get some changes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes'
+import { prisma } from './lib/prisma'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.get("/", async (_req, res) => {
+  const now = await prisma.$queryRaw`SELECT NOW()`;
+  res.json({ message: "Backend running 🚀", now });
+});
 app.use('/api/v1', routes)
 export default app
