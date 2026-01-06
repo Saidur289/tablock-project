8e38b9285796766cc52a344f9b41f63dd14c20b3
Saidur289 <saidur.riaz1@gmailcomm>
2026-01-07 01:22:53 +0600

    change

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes/index.js'
-import { prisma } from './lib/prisma'
+import { prisma } from './lib/prisma.js'
 
 const app = express()
 app.use(express.json())
